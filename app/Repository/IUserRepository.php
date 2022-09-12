188de4fa424aec580133befcb24ac66cfa0907a2
Eaulmesse <theo.guerin01120@gmail.com>
2022-09-12 16:45:28 +0200

    feat: IUserRepository op

--- app/Repository/IUserRepository.php
+++ app/Repository/IUserRepository.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+
+interface IUserRepository
+{
+    public function add(User $user);
+
+    public function findAll() : array;
+
+    public function findByNom(string $nom): User;
+
+    public function update(User $user);
+
+    public function remove(User $user);
+
+    public function findById($params): User;
+
+    public function findByMail(string $mail);
+}
