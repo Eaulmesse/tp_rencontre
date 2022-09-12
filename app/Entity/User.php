832a9d2ccacf9aa37b5df7ac052f064d4ea1265d
Eaulmesse <theo.guerin01120@gmail.com>
2022-09-12 16:08:02 +0200

    feat: user.php op

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -1,1 +1,98 @@
 <?php
+
+
+namespace App\Entity;
+
+class User
+{
+    private int $id;
+    private string $nom;
+    private string $prenom;
+    private string $departement;
+    private string $mail;
+
+    public function __construct($nom,$prenom,$mail,$departement)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMail(): string
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @param string $mail
+     */
+    public function setMail(string $mail): void
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDepartement(): string
+    {
+        return $this->departement;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @param string $prenom
+     */
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @param string $departement
+     */
+    public function setDepartement(string $departement): void
+    {
+        $this->departement = $departement;
+    }
+}
